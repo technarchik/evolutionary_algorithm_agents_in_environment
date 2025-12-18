6aa6a0932b3e509ed00d8fb7b926423a248097d6
technarchik <95281889+technarchik@users.noreply.github.com>
2025-12-19 01:26:25 +0300

    Crossover alg

--- Assets/Scripts/GeneticAlgorithm1.cs
+++ Assets/Scripts/GeneticAlgorithm1.cs
@@ -83,22 +83,159 @@ public class GeneticAlgorithm1 : MonoBehaviour
     #endregion
 
 
-    #region GA algorithm
+    #region Natural Selection
 
     #endregion
 
 
-    #region Fitness function
+    #region GA algorithm
+    // method for evolving classes from Animal - doesnt matter what type
+    List<List<float>> EvolveAnimals<T>(List<T> animals) where T : Animal 
+    {
+        // fitness (before it) -> sorting -> elitism -> crossover -> mutation
+        // sorting
+        animals.Sort((a, b) => b.score.CompareTo(a.score));
+        List<List<float>> children = new List<List<float>>();
 
+        // elitism
+        for (int i = 0; i < elitismCount; i++)
+        {
+            children.Add(TakeGenesFromAnimal(animals[i]));
+        }
+
+        // crossover (needed tournament selection)
+        while (children.Count < animals.Count)
+        {
+            // selecting parents
+            Animal mother = TournamentSelection(animals);
+            Animal father = TournamentSelection(animals);
+
+            // remember their genes
+            var genesMother = TakeGenesFromAnimal(mother);
+            var genesFather = TakeGenesFromAnimal(father);
+
+            // crossover mechanism
+            if (Random.value < crossoverRate)
+                CrossoverAlgorithm(genesMother, genesFather);
+
+            // mutation mechanism
+            Mutation(genesMother);
+            Mutation(genesFather);
+
+            // adding genes to the next generation
+            children.Add(genesMother);
+            if (children.Count < animals.Count)    // idk should i do this way?
+                children.Add(genesFather);
+        }
+        return children;
+    }
+
+    List<float> TakeGenesFromAnimal(Animal animal)
+    {
+        List<float> geneList = new List<float> { animal.stamina, animal.speed, animal.temp_resist, animal.wet_resist, animal.eat_need};
+        return geneList;
+    }
+
+    Animal TournamentSelection<T>(List<T> list) where T : Animal
+    {
+        Animal bestAnimal = null;
+        // may be can write this method another way
+        for (int i = 0; i < tournamentSize; i++) 
+        {
+            var c = list[Random.Range(0, list.Count)];
+            if (bestAnimal == null || c.score > bestAnimal.score)
+                bestAnimal = c;
+        }
+        return bestAnimal;
+    }
+
+    #endregion
+
+
+    #region Fitness functions
+
+    void EvaluateFitnessPredator()
+    {
+        foreach (var predator in predators)
+        {
+            // must count, what important for predators?
+        }
+    }
+
+    void EvaluateFitnessHerbivore()
+    {
+        foreach(var herbivore in herbivores)
+        {
+            // must count, what important for herbivores?
+        }
+    }
+
+    void EvaluateFitnessInEnvironment(Animal animal)
+    {
+        float tempFit;
+        float wetFit;
+    }
     #endregion
 
 
     #region Crossover algorithm
 
+    void CrossoverAlgorithm(List<float> mother, List<float> father)
+    {
+        // VARIANT 1: single point crossover
+        int point = Random.Range(1, mother.Count);
+        for (int i = point; i < mother.Count; i++)
+        {
+            float temp = mother[i];
+            mother[i] = father[i];
+            father[i] = temp;
+        }
+    }
+
     #endregion
 
 
     #region Mutation algorithm
+
+    void Mutation(List<float> gene)
+    {
+        for (int i = 0; i < gene.Count; i++)
+        {
+            if (Random.value < mutationRate)
+            {
+                // VARIANT 1: without checking allowed interval
+                // gene[i] += Random.Range(-0.1f, 0.1f); 
+
+                // VARIANT 2: with checking allowed interval
+                float noise = Random.Range(-0.1f, 0.1f);
+                gene[i] += noise;
+
+                switch(i)
+                {
+                    case 0: // stamina
+                        gene[i] = Mathf.Clamp(gene[i], staminaRange.min, staminaRange.max);
+                        break;
+                    case 1: // speed
+                        gene[i] = Mathf.Clamp(gene[i], speedRange.min, speedRange.max);
+                        break;
+                    case 2: // temp_resist
+                        gene[i] = Mathf.Clamp(gene[i], tempResistRange.min, tempResistRange.max);
+                        break;
+                    case 3: // wet_resist
+                        gene[i] = Mathf.Clamp(gene[i], wetResistRange.min, wetResistRange.max);
+                        break;
+                    case 4: //eat_need
+                        gene[i] = Mathf.Clamp(gene[i], eatNeedRange.min, eatNeedRange.max);
+                        break;
+
+                }
+
+                // VARIANT 3: mutaion adapting to difficultyMode of the env
+                // float envFactor = env.difficultyMode == Environment.DifficultyMode.hardMode ? 0.2f : 0.05f;
+                // float delta = Random.Range(-envFactor, envFactor);
+            }
+        }
+    }
 
     #endregion
 
